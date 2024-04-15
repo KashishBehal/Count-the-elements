27a937a951e1d24fffb04557978f1ad2318ac092
Kashish Behal <113112253+KashishBehal@users.noreply.github.com>
2024-04-15 12:12:09 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<int> countElements(vector<int> &a, vector<int> &b, int n, vector<int> &query,
+                              int q) {
+       
+  sort(b.begin(),b.end());
+        vector<int>ans;
+        for(int i=0;i<q;i++)
+        {
+            int x=a[query[i]];
+            int y=upper_bound(b.begin(),b.end(),x)-b.begin();
+            ans.push_back(y);
+        }
+        return ans;
+    
+    }
+};
